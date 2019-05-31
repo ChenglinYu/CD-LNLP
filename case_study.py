96960a42edab485fb65ca2de2e024b814ddfaca9
chenglinyu <y2607570419@gmail.com>
2019-05-31 11:59:22 +0800

    reconstruct case study on Dataset2

--- case_study.py
+++ case_study.py
@@ -3,14 +3,21 @@
 # @Date  : 2019/3/2
 # @Desc  :
 
-import pandas as pd
 import LNLP_method
 import numpy as np
-import input_and_output
+import pandas as pd
+import csv
+
+# write matrix to csv file
+def matrix_to_csv(matrix, file_name):
+    arrays = np.array(matrix)
+    arrays_frame = pd.DataFrame(arrays)
+    arrays_frame.to_csv(file_name, header=None, index=None)
+
 
 if __name__ == '__main__':
-    association_data = pd.read_csv('data/association.csv', header=None).values
+    association_data = pd.read_csv('Dataset2/association.csv', header=None).values
     score_matrix = LNLP_method.linear_neighbor_predict(association_data,alpha=0.1,neighbor_rate=0.9,
                                                        circRNA_weight=1.0)
     score_matrix[np.where(association_data == 1)] = 0
-    input_and_output.matrix_to_csv(score_matrix, 'produced_data/scores.csv')
+    matrix_to_csv(score_matrix, 'case_study_scores/Dataset2_scores.csv')
